1af11d0128adb9b132951207fa647d311b520825
shunm-999 <shunmiyazaki7812@gmail.com>
2023-12-17 15:25:27 +0900

    update Gradle Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    kotlin("jvm") version "1.9.21" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.9.21"))
+    }
 }
